c6b183051d513512ebdcd48f146d1804276a85a8
MAvi-Rup <avirupmondal19@gmail.com>
2022-05-06 21:19:23 +0600

    Heroku Deploy

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { decoded } = require("jsonwebtoken");
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -10,17 +12,50 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function verifyToken(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token,process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' })
+        }
+        console.log('decoded', decoded)
+        req.decoded = decoded;
+        next();
+
+    })
+
+   
+}
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.avjko.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         const serviceCollection = client.db('genius-car').collection('service');
 
+        const orderCollection = client.db('genuis-car').collection('order')
+
+        //AUTH
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, {
+                expiresIn: '1d'
+            });
+            res.send(accessToken)
+        })
+
+
+
         //All Data Load
-        app.get('/service', async(req,res)=>{
+        app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query)
             const services = await cursor.toArray();
@@ -28,32 +63,60 @@ async function run(){
         })
         // Single data load
 
-        app.get('/service/:id',async(req,res)=>{
+        app.get('/service/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)}
+            const query = { _id: ObjectId(id) }
             const service = await serviceCollection.findOne(query)
             res.send(service)
         })
 
         // POST A SERVICE
 
-        app.post('/service',async(req,res)=>{
+        app.post('/service', async (req, res) => {
             const newService = req.body;
             const result = await serviceCollection.insertOne(newService)
             res.send(result)
 
         })
 
+
+        //Order collection Api
+
+        app.post('/order', async (req, res) => {
+            // const decodedEmail = req.decoded.email;
+
+            const order = req.body;
+            const result = await orderCollection.insertOne(order)
+            res.send(result)
+
+        })
+
+        //Get all order Api
+
+        app.get('/order', verifyToken, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            if (email == decodedEmail) {
+                const query = { email: email }
+                const cursor = orderCollection.find(query)
+                const order = await cursor.toArray()
+                res.send(order)
+            }else{
+                res.status(403).send({message: 'Forbidden'})
+            }
+
+        })
+
         //Delete API 
 
-        app.delete('/service/:id',async(req,res)=>{
+        app.delete('/service/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id:ObjectId(id)}
+            const query = { _id: ObjectId(id) }
             const result = await serviceCollection.deleteOne(query);
             res.send(result)
         })
     }
-    finally{
+    finally {
 
     }
 
@@ -64,13 +127,13 @@ run().catch(console.dir)
 
 
 //server starting default page
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("Genius Server")
 })
 
 //server starting 
 
-app.listen(port,()=>{
-    console.log('Listening from port',port)
+app.listen(port, () => {
+    console.log('Listening from port', port)
 })
 
